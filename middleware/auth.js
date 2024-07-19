fcd465e723707c3a5b18d4f2b61b6f213716bf0e
Pramod Naik <93259889+Pramod26naik@users.noreply.github.com>
2024-07-19 22:38:27 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken'); 
 const User = require('../models/User');
-
+//authenticate users
 const authenticateUser = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
