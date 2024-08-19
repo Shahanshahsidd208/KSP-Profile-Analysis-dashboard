f50e249771c717bb38c4ae69a860bc08262431f5
Shahanshah Siddiqui <105127757+Shahanshahsidd208@users.noreply.github.com>
2024-08-19 13:25:29 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -139,7 +139,7 @@ export default function Home({ language }) {
   };
 
   //chatbot end
-
+// function to handle different login 
   const handleProfileClick = () => {
     setLoginType("profile");
   };
